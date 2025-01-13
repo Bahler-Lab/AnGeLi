fa3d9349887202716adef2a916848efa490b8b2b
Jake <jake@saxonconsulting.net>
2025-01-13 00:08:50 +0000

    Added skeleton functions and started tests

--- angeli.py
+++ angeli.py
@@ -41,94 +41,100 @@ class ProteinComposition(Enum):
     V = 18
     W = 19
     Y = 20
-    
-def download_file(url):
-    """
-    Downloads a file from a URL and returns the content as a BytesIO object.
 
-    :param url: The URL of the file.
-    :return: A BytesIO object containing the content.
-    """
-    # Send a GET request to the URL
-    response = requests.get(url)
+class AnGeLi:
+    def __init__(self):
+        """Empty Constructor"""
 
-    if response.status_code == 200:
-        # Return the content as a BytesIO object
-        return io.BytesIO(response.content)
-    else:
-        print(f"Failed to download the file. Status code: {response.status_code}")
-        return None
+    def download_file(self, url):
+        """
+        Downloads a file from a URL and returns the content as a BytesIO object.
 
-def download_and_decompress_gzip(url):
-    """
-    Downloads a file from a URL, decompresses it using gzip, and returns the decompressed content as a BytesIO object.
+        :param url: The URL of the file.
+        :return: A BytesIO object containing the content.
+        """
+        # Send a GET request to the URL
+        response = requests.get(url)
 
-    :param url: The URL of the gzip-compressed file.
-    :return: A BytesIO object containing the decompressed content.
-    """
-    # Send a GET request to the URL
-    response = requests.get(url, stream=True)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Create a GzipFile object from the downloaded content
-        with gzip.GzipFile(fileobj=io.BytesIO(response.content), mode='rb') as gz:
-            # Read the decompressed content into a BytesIO object
-            decompressed_content = io.BytesIO(gz.read())
+        if response.status_code == 200:
+            # Return the content as a BytesIO object
+            return io.BytesIO(response.content)
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
+            return None
+
+    def download_and_decompress_gzip(self, url):
+        """
+        Downloads a file from a URL, decompresses it using gzip, and returns the decompressed content as a BytesIO object.
+
+        :param url: The URL of the gzip-compressed file.
+        :return: A BytesIO object containing the decompressed content.
+        """
+        # Send a GET request to the URL
+        response = requests.get(url, stream=True)
         
-        print("File successfully downloaded and decompressed.")
-        return decompressed_content
-    else:
-        print(f"Failed to download the file. Status code: {response.status_code}")
-        return None
-    
-def parse_tsv(file_obj):
-    """
-    Parses a tab-separated values (TSV) file and returns the content as a list of dictionaries.
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Create a GzipFile object from the downloaded content
+            with gzip.GzipFile(fileobj=io.BytesIO(response.content), mode='rb') as gz:
+                # Read the decompressed content into a BytesIO object
+                decompressed_content = io.BytesIO(gz.read())
+            
+            print("File successfully downloaded and decompressed.")
+            return decompressed_content
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
+            return None
+        
+    def parse_tsv(self, file_obj):
+        """
+        Parses a tab-separated values (TSV) file and returns the content as a list of dictionaries.
 
-    :param url: The file object containing the TSV content.
-    :return: A list of dictionaries representing the TSV content.
-    """
-    # Open the TSV file
-    # Reset the BytesIO stream's position to the beginning if it was previously read
-    file_obj.seek(0)
-    
-    # Create a CSV reader object with a tab delimiter
-    reader = csv.DictReader(file_obj, delimiter='\t')
-    return [row for row in reader]
+        :param url: The file object containing the TSV content.
+        :return: A list of dictionaries representing the TSV content.
+        """
+        # Open the TSV file
+        # Reset the BytesIO stream's position to the beginning if it was previously read
+        file_obj.seek(0)
+        
+        # Create a CSV reader object with a tab delimiter
+        reader = csv.DictReader(file_obj, delimiter='\t')
+        return [row for row in reader]
 
-def parse_gff3(file_obj):
-    """
-    Parses a GG3 file and returns the features.
+    def parse_gff3(self, file_obj):
+        """
+        Parses a GG3 file and returns the features.
 
-    :param url: The gff file object
-    :return: A gffutils database object
-    """
-    # Create a database from the GFF3 file object
-    db = gffutils.create_db(file_obj, dbfn=':memory:', force=True, keep_order=True, merge_strategy='merge', sort_attribute_values=True)
-    
-    return db
+        :param url: The gff file object
+        :return: A gffutils database object
+        """
+        # Create a database from the GFF3 file object
+        db = gffutils.create_db(file_obj, dbfn=':memory:', force=True, keep_order=True, merge_strategy='merge', sort_attribute_values=True)
+        
+        return db
 
-def build_file_headers():
-    print("Building file headers")
+    def build_file_headers(self):
+        print("Building file headers")
 
-def build_protein_features():
-    print("Building protein features")
+    def build_protein_features(self):
+        print("Building protein features")
 
-def build_composition_features():
-    print("Building composition features")
+    def build_composition_features(self):
+        print("Building composition features")
 
-def build_chromosome_features():
-    print("Building chromosome features")
+    def build_chromosome_features(self):
+        print("Building chromosome features")
 
-def build_go_terms():
-    print("Building GO terms")
+    def build_go_terms(self):
+        print("Building GO terms")
 
-def build_fypo_terms():
-    print("Building FYPO terms")
+    def build_fypo_terms(self):
+        print("Building FYPO terms")
 
-def parse_original_AnGeLiDatabase():
-    print("Parsing the original AnGeLiDatabase.txt file")
+    def parse_original_AnGeLiDatabase(self):
+        print("Parsing the original AnGeLiDatabase.txt file")
+
+
 
 def main():
     """
--- angeli_test.py
+++ angeli_test.py
@@ -1,3 +1,27 @@
 # Test the files to make sure we have the enums correct.
 # All files are located in /test_data
+import unittest
+from angeli import AnGeLi
 
+class TestAnGeLi(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the test environment."""
+        # Create a new MyClass instance for testing
+        self.db = AnGeLi()
+
+    def read_test_tsv(self, file_name):
+        """Read a test TSV file and return the content as a list of dictionaries."""
+        # Open the test TSV file
+        with open(f"test_data/{file_name}", "rb") as file:
+            # Parse the TSV file using the parse_tsv method
+            return self.db.parse_tsv(file)
+
+    def test_peptides(self):
+        """Test the greet function."""
+        # Expected greeting string
+        expected_greeting = "Hello, my name is Alice and I am 30 years old."
+        # Actual greeting from the greet function
+        actual_greeting = self.person.greet()
+        # Assert that the actual greeting matches the expected greeting
+        self.assertEqual(actual_greeting, expected_greeting)
